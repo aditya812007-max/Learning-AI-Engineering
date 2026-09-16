7839597f041898f5a0db0b4cb9a7457ebb34c1dd
Aditya Biswal <aditya812007@gmail.com>
2026-09-17 01:17:22 +0530

    fix(cors): add portfolio URL to allowed origins

--- week2/day10/backend/adi.py
+++ week2/day10/backend/adi.py
@@ -185,6 +185,7 @@ app.add_middleware(
     CORSMiddleware,
 
     allow_origins=[
+        "https://aditya-biswal-portfolio.vercel.app/"
         "http://127.0.0.1:5500",
         "http://localhost:5500",
         "http://localhost:3000",
